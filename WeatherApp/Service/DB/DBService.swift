e4c1c0f53e458d539b2e5119267a6367b2f15958
anasmatn <42275844+anasamer1997@users.noreply.github.com>
2024-09-02 20:07:25 +0300

    update DBservice
    add new class to use it and save and load from core data

--- WeatherApp/Service/DB/DBService.swift
+++ WeatherApp/Service/DB/DBService.swift
@@ -6,11 +6,76 @@
 //
 
 import Foundation
+import CoreData
+
+
+protocol CoreDataStorageServiceProtocol {
+    func saveWeatherFromLocalStorage(_ weather: Hourly)
+    func loadWeatherInLocalStorage() -> Hourly?
+}
 
 protocol LocalStorageServiceProtocol {
-    func save<T: Codable>(data: T, forKey key: String) -> Bool 
+    func save<T: Codable>(data: T, forKey key: String) -> Bool
     func load<T: Codable>(forKey key: String, as type: T.Type) -> T?
 }
+
+class CoreDataStorageService: CoreDataStorageServiceProtocol {
+    
+    static let shared = CoreDataStorageService()
+    private init() {}
+    
+    var context: NSManagedObjectContext {
+        return persistentContainer.viewContext
+    }
+    
+    private lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "HourlyCoreData") // Replace with your Core Data model name
+        container.loadPersistentStores { storeDescription, error in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        }
+        return container
+    }()
+    
+    
+    func saveWeatherFromLocalStorage(_ weather: Hourly) {
+        let request: NSFetchRequest<HourlyEntity> = HourlyEntity.fetchRequest()
+        do {
+            let results = try context.fetch(request)
+            let hourlyEntity = results.first ?? HourlyEntity(context: context)
+            
+            hourlyEntity.time = weather.time as NSObject
+            hourlyEntity.temperature2M = weather.temperature2M as NSObject
+            hourlyEntity.weatherCode = weather.weatherCode as NSObject
+            
+            try context.save()
+            print("Weather data saved successfully")
+        } catch {
+            print("Error saving weather data: \(error.localizedDescription)")
+        }
+    }
+    
+    func loadWeatherInLocalStorage() -> Hourly? {
+        let request: NSFetchRequest<HourlyEntity> = HourlyEntity.fetchRequest()
+        do {
+            guard let result = try context.fetch(request).first else {
+                print("No weather data found in local storage")
+                return nil
+            }
+            let weatherTime = result.time as! [String]
+            let weatherTemp = result.temperature2M as! [Double]
+            let weatherCode = result.weatherCode as! [Int]
+            
+            let hourlyModel = Hourly(time: weatherTime, temperature2M: weatherTemp, weatherCode: weatherCode)
+            return hourlyModel
+        } catch {
+            print("Error loading weather data: \(error.localizedDescription)")
+            return nil
+        }
+    }
+}
+
 class LocalStorageService:LocalStorageServiceProtocol{
     
     static let shared = LocalStorageService()
